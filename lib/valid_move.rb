de17ae9943f6d522833e7de07c7c68222ffe4d45
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 09:15:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 def valid_move(board=[" "," "," "," "," "," "," "," "," "],index=0)
   if position_taken?(board,index)
     return false
+  elsif index>8
+    return false
   end
   return true
 end
