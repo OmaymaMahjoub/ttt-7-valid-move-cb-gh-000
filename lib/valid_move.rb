fe23e060eafcc48db3809ec8bf251e7294811eca
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 09:12:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,9 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(arr=[" "," "," "," "," "," "," "," "," "],index=0)
+  if (arr[index]=="O")||(arr[index]=="X")
+    return true
+  end
+  return false
+end
