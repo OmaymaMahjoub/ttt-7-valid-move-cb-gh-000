fb625ff7a342935ddc9511d7f623ac9268190bd7
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 09:14:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move(board=[" "," "," "," "," "," "," "," "," "],index=0)
+  if position_taken?(board,index)
+    return false
+  end
+  return true
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(arr=[" "," "," "," "," "," "," "," "," "],index=0)
